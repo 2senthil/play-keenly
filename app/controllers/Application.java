728c1c318e12735a5ce68993d637d71ec492c864
Senthil Sundaram <k.s.senthil@gmail.com>
2014-08-24 23:29:54 -0700

    Remove db. Hardcode skills for API verification

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,6 +8,7 @@ import play.mvc.*;
 
 import views.html.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static play.libs.Json.toJson;
@@ -19,13 +20,22 @@ public class Application extends Controller {
     }
 
     public static Result addSkill() {
-        Skill skill = Form.form(Skill.class).bindFromRequest().get();
-        skill.save();
-        return redirect(routes.Application.index());
+        //Skill skill = Form.form(Skill.class).bindFromRequest().get();
+        //skill.save();
+        return redirect(controllers.routes.Application.index());
     }
 
     public static Result getSkills() {
-        List<Skill> skills = new Model.Finder(String.class, Skill.class).all();
+        //List<Skill> skills = new Model.Finder(String.class, Skill.class).all();
+        List<Skill> skills = new ArrayList<Skill>();
+        Skill skillOne = new Skill();
+        skillOne.id = "1";
+        skillOne.name = "Java";
+        skills.add(skillOne);
+        Skill skillTwo = new Skill();
+        skillTwo.id = "2";
+        skillTwo.name = "Scala";
+        skills.add(skillTwo);
         return ok(toJson(skills));
     }
 }
--- app/models/Skill.java
+++ app/models/Skill.java
@@ -1,14 +1,16 @@
 package models;
 
+/*
 import play.db.ebean.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+*/
 
-@Entity
-public class Skill extends Model {
-
-    @Id
+//@Entity
+//public class Skill extends Model {
+public class Skill {
+//    @Id
     public String id;
 
     public String name;
